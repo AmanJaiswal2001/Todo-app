94d2ce20f596c9375f4c9b5a085c5fa1e95117d2
AmanJaiswal2001 <amanjaisawal537>
2024-08-01 16:18:31 +0530

    my todo app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+  
+
+  return (
+    <>
+   <h1>hii todo</h1>
+   <AddTodo/>
+   <Todos/>
+    </>
+  )
+}
+
+export default App
